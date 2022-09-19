27988c4d7d71a9a79996a565e9c043b2a65d88f8
l1can <a.a.soroka05@gmail.com>
2022-09-19 18:30:27 +0400

    trying to fix classes

--- studhelper.py
+++ studhelper.py
@@ -1,24 +1,7 @@
 import telebot
-import pymysql.cursors
-from config import host, user, password, db_name
+from team import Team
+from user import User
 from telebot import types
-
-
-# Функция для подключения к базе данных
-def connect_to_db():
-    try:
-        connection = pymysql.connect(
-            host=host,
-            port=3306,
-            user=user,
-            password=password,
-            database=db_name,
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        print("Connected successfully!")
-        return connection
-    except Exception as ex:
-        print("Connection error!", ex)
 
 
 token = "5102428240:AAF-GZ5AbcbYVPlCnBG_qwFCrhLiWIPgXIE"
@@ -26,13 +9,17 @@ bot = telebot.TeleBot(token)
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup_after_creating_team = types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup_yes_no = types.ReplyKeyboardMarkup(resize_keyboard=True)
+username = ''
+user = User()
+team = Team()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     item1 = types.KeyboardButton("Регистрация команды")
     markup.add(item1)
-    bot.send_message(message.chat.id, "Привет " + message.from_user.username, reply_markup=markup)
+    username = message.from_user.username
+    bot.send_message(message.chat.id, "Привет " + username, reply_markup=markup)
 
 
 @bot.message_handler(commands=['button'])
@@ -48,18 +35,6 @@ def message_reply(message):
         bot.register_next_step_handler(msg, after_name_of_team_text)  # после сообщения от юзера переходим в функцию
 
 
-# Функция для вставки новой команды в базу данных
-def insert_new_team(teamname, username):
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            sql_request = "INSERT INTO `Команды` (`Название`, `Администратор`) VALUES (%s, %s)"
-            cursor.execute(sql_request, (teamname, username))
-            connection.commit()
-    finally:
-        connection.close()
-
-
 def after_name_of_team_text(message):
     item1 = types.KeyboardButton("Удалить команду")
     markup_after_creating_team.add(item1)
@@ -67,7 +42,10 @@ def after_name_of_team_text(message):
     msg = bot.send_message(message.chat.id, "Имя вашей команды - " + message.text,
                            reply_markup=markup_after_creating_team)  # в message.text хранится то, что написал чел
 
-    insert_new_team(message.text, message.from_user.username)
+    print(username, 0)
+    team = Team(message.text, user.get_username())
+
+    team.add()
 
     bot.register_next_step_handler(msg, after_team_registered_text)
 
@@ -78,35 +56,11 @@ def after_team_registered_text(message):
         bot.register_next_step_handler(msg, after_team_delete_text)
 
 
-# Функция для удаления определенной команды из базы данных
-def delete_team(teamname):
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            sql_request = "DELETE FROM `Команды` WHERE `Название` = %s"
-            cursor.execute(sql_request, teamname)
-            connection.commit()
-    finally:
-        connection.close()
-
-
-def get_team_by_username(username):
-    connection = connect_to_db()
-    try:
-        with connection.cursor() as cursor:
-            sql_request = "SELECT `Название` FROM `Команды` WHERE `Администратор` = %s"
-            cursor.execute(sql_request, username)
-            teamname = cursor.fetchone()
-    finally:
-        connection.close()
-    return teamname
-
 
 def after_team_delete_text(message):
     if message.text == "Да":
-        teamname = get_team_by_username(message.from_user.username)
         bot.send_message(message.chat.id, "Удаляю вашу команду ")
-        delete_team(teamname['Название'])
+        team.delete()
     elif message.text == "Нет":
         bot.send_message(message.chat.id, "Хорошо")
 
--- user.py
+++ user.py
@@ -1,7 +1,13 @@
 class User:
-    def __init__(self, username):
-        self.teamname = None
-        self.username = username
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.username = args[0]
+        elif len(args) == 2:
+            self.username = args[0]
+            self.teamname = args[1]
+        else:
+            self.username = None
+            self.teamname = None
 
     def set_team(self, teamname):
         self.teamname = teamname
@@ -9,3 +15,8 @@ class User:
     def get_team(self):
         return self.teamname
 
+    def set_username(self, username):
+        self.username = username
+
+    def get_username(self):
+        return self.username
