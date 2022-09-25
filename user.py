f039e25bcf2f79dd712dc878b1b8ffc649bfd460
kristina03g <kkristina03g@yandex.ru>
2022-09-25 23:14:34 +0400

    Изменила класс User, чтобы заполнялись все 5 полей таблицы из БД. Добавила ко всему геттеры и сеттеры, немного изменила метод Димы с добавлением пользователя, так как поменяла поля у User, и добавила удаление пользователя из БД (подумала, что логичнее всего удалять по username, т.к. остальные поля могут совпадать у нескольких пользователей)

--- user.py
+++ user.py
@@ -19,19 +19,43 @@ def connect_to_db():
 class User:
     def __init__(self, *args):
         if len(args) == 1:
-            self.username = args[0]
+            self.name = args[0]
         elif len(args) == 2:
-            self.username = args[0]
-            self.teamname = args[1]
+            self.name = args[0]
+            self.surname = args[1]
+        elif len(args) == 3:
+            self.name = args[0]
+            self.surname = args[1]
+            self.username = args[2]
+        elif len(args) == 4:
+            self.name = args[0]
+            self.surname = args[1]
+            self.username = args[2]
+            self.teamname = args[3]
+        elif len(args) == 5:
+            self.name = args[0]
+            self.surname = args[1]
+            self.username = args[2]
+            self.teamname = args[3]
+            self.role = args[4]
         else:
+            self.name = None
+            self.surname = None
             self.username = None
             self.teamname = None
+            self.role = None
 
-    def set_team(self, teamname):
-        self.teamname = teamname
+    def set_name(self, name):
+        self.name = name
 
-    def get_team(self):
-        return self.teamname
+    def get_name(self):
+        return self.name
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_surname(self):
+        return self.surname
 
     def set_username(self, username):
         self.username = username
@@ -39,12 +63,34 @@ class User:
     def get_username(self):
         return self.username
 
+    def set_teamname(self, teamname):
+        self.teamname = teamname
+
+    def get_teamname(self):
+        return self.teamname
+
+    def set_role(self, role):
+        self.role = role
+
+    def get_role(self):
+        return self.role
+
     def add_user(self):
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                insert_user = "INSERT INTO `Пользователи` (`Имя`, `Фамилия`, `user_name`, `team`, `role`) VALUES (%s, %s, %s, %s, %s);"
-                cursor.execute(insert_user) # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
+                insert_user = "INSERT INTO `Пользователи` (`Имя`, `Фамилия`, `User_name`, `Команда`, `Роль`) VALUES (%s, %s, %s, %s, %s);"
+                cursor.execute(insert_user, (self.name, self.surname, self.username, self.teamname, self.role)) # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
+                connection.commit()
+        finally:
+            connection.close()
+
+    def delete_user(self):
+        connection = connect_to_db()
+        try:
+            with connection.cursor() as cursor:
+                delete_user = "DELETE FROM `Пользователи` WHERE `User_name` = %s"
+                cursor.execute(delete_user, self.username)
                 connection.commit()
         finally:
             connection.close()
