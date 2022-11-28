e51ce0e9e6abcd99de5fbfbc2382caf31c896426
kristina03g <kkristina03g@yandex.ru>
2022-11-28 10:45:00 +0400

    Добавила работу с базой данных на странице с командой, но не добавляются данные из бд

--- web-admin/teams.php
+++ web-admin/teams.php
@@ -1,10 +1,10 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$par1_ip = "server136.hosting.reg.ru";
-$par2_name = "u1789997";
-$par3_p = "v9VFX935Zqj8x05Hp";
-$par4_db = "u1789997_studhelper";
+$par1_ip = "server241.hosting.reg.ru";
+$par2_name = "u1841284";
+$par3_p = "gcb6Z3o89KTLkg1I";
+$par4_db = "u1841284_studhelper";
 
 $mysqli = new mysqli($par1_ip, $par2_name, $par3_p, $par4_db);
 mysqli_set_charset($mysqli,'utf8');
