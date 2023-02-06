315669f3cc37e741bc0b93408450f3c26dd822e5
l1can13 <a.a.soroka05@gmail.com>
2023-02-06 14:51:00 +0400

    сделал, чтобы работала регистрация и добавление пользователей в команду, возможны новые баги, нужно проверить

--- user.py
+++ user.py
@@ -60,9 +60,11 @@ class User:
             self.username = args[3]
             self.teamname = args[4]
             self.role = args[5]
-            self.id = args[6]
+            self.tg_id = args[6]
             self.counter_of_people = 0
-
+            self.invite_code = None
+            self.db_id = None
+            self.team_id = None
         else:
             self.name = None
             self.surname = None
@@ -70,16 +72,14 @@ class User:
             self.username = None
             self.teamname = None
             self.role = None
-            self.id = 0
+            self.tg_id = 0
             self.counter_of_people = 0
-        # self.report = None
-        # self.departure_time = None
 
     def find_username_by_surname(self, surname):
         connection = connect_to_db()
         try:
             with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-                #sql_request = "SELECT `Ид` FROM `Пользователи` WHERE `Имя` = %s"  # строка для SQL-запроса
+                # sql_request = "SELECT `Ид` FROM `Пользователи` WHERE `Имя` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `user_id` FROM `users` WHERE `name` = %s"  # строка для SQL-запроса
                 cursor.execute(sql_request, surname)
                 result = cursor.fetchone()
@@ -87,6 +87,39 @@ class User:
                 return result['Ид']
         finally:
             connection.close()
+
+    def set_db_id(self):
+        connection = connect_to_db()
+        try:
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                sql_request = "SELECT `user_id` FROM `users` WHERE `tg_id` = %s"  # строка для SQL-запроса
+                cursor.execute(sql_request, self.tg_id)
+                result = cursor.fetchone()
+                connection.commit()
+                if result:
+                    self.db_id = result['user_id']
+                else:
+                    self.db_id = -1
+        finally:
+            connection.close()
+
+    def get_db_id_from_db(self):
+        connection = connect_to_db()
+        try:
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                sql_request = "SELECT `user_id` FROM `users` WHERE `tg_id` = %s"  # строка для SQL-запроса
+                cursor.execute(sql_request, self.tg_id)
+                result = cursor.fetchone()
+                connection.commit()
+                return result['user_id']
+        finally:
+            connection.close()
+
+    def set_invite_code(self, invite_code):
+        self.invite_code = invite_code
+
+    def get_invite_code(self):
+        return self.invite_code
 
     def get_counter_of_people(self):
         return self.counter_of_people
@@ -118,6 +151,21 @@ class User:
     def get_username(self):
         return self.username
 
+    def set_team_id(self):
+        connection = connect_to_db()
+        try:
+            with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+                sql_request = "SELECT `team_id` FROM `teams` WHERE `team_name` = %s"  # строка для SQL-запроса
+                cursor.execute(sql_request, self.teamname)
+                result = cursor.fetchone()
+                connection.commit()
+                self.team_id = result['team_id']
+        finally:
+            connection.close()
+
+    def get_team_id(self):
+        return self.team_id
+
     def set_teamname(self, teamname):
         self.teamname = teamname
 
@@ -130,36 +178,49 @@ class User:
     def get_role(self):
         return self.role
 
-    def set_id(self, arg):
-        self.id = arg
+    def set_tg_id(self, arg):
+        self.tg_id = arg
 
-    def get_id(self):
-        return self.id
+    def get_tg_id(self):
+        return self.tg_id
 
-    # def get_report(self):
-    #     return  self.report
-    #
-    # def set_report(self,arg):
-    #     self.report = arg
-    #
-    # def get_departure_time(self):
-    #     return self.departure_time
-    #
-    # def set_departure_time(self, arg):
-    #     self.departure_time = arg
+    def get_db_id(self):
+        return self.db_id
 
     def add_user(self):
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #insert_user = "INSERT INTO `Пользователи` (`Имя`, `Группа`, `User_name`, `Команда`, `Роль`, `Ид`) VALUES (%s, %s, %s, %s, %s, %s);"
-                insert_user = "INSERT INTO `users` (`user_id`, `name`, `tg_username`) VALUES (%s, %s, %s);"
-                cursor.execute(insert_user, (self.id, self.name, self.username)) # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
+                # insert_user = "INSERT INTO `Пользователи` (`Имя`, `Группа`, `User_name`, `Команда`, `Роль`, `Ид`) VALUES (%s, %s, %s, %s, %s, %s);"
+                insert_user = "INSERT INTO `users` (`tg_id`) VALUES (%s);"
+                cursor.execute(insert_user,
+                               self.tg_id)  # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
                 connection.commit()
 
-                #insert_user = "INSERT INTO `team_members` (`team_id`, `user_id`, `role`, `invite_code`, `Роль`, `Ид`) VALUES (%s, %s, %s, %s, %s, %s);"
-                #cursor.execute(insert_user, (self.name, self.group, self.username, self.teamname, self.role, self.id))  # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
-                #connection.commit()
+                self.set_db_id()
+        finally:
+            connection.close()
+
+    def add_user_to_team_members(self):
+        self.set_team_id()
+        connection = connect_to_db()
+        try:
+            with connection.cursor() as cursor:
+                insert_user = "INSERT INTO `team_members` (`team_id`, `user_id`, `role`, `invite_code`) VALUES (%s, %s, %s, %s);"
+                cursor.execute(insert_user, (self.team_id, self.db_id, self.role,
+                                             self.invite_code))  # cursor.execute(insert_user, (Сюда переменные через запятую, которые надо добавть в таблицу.))
+                connection.commit()
+        finally:
+            connection.close()
+
+    def update_user_id_in_team_members(self):
+        self.set_db_id()
+        connection = connect_to_db()
+        try:
+            with connection.cursor() as cursor:
+                insert_user = "UPDATE `team_members` SET `user_id` = %s WHERE `invite_code` = %s"
+                cursor.execute(insert_user, (self.db_id, self.invite_code))
+                connection.commit()
         finally:
             connection.close()
 
@@ -167,13 +228,13 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #delete_user = "DELETE FROM `Пользователи` WHERE `Ид` = %s"
+                # delete_user = "DELETE FROM `Пользователи` WHERE `Ид` = %s"
                 delete_user = "DELETE FROM `users` WHERE `user_id` = %s"
-                cursor.execute(delete_user, self.id)
+                cursor.execute(delete_user, self.db_id)
                 connection.commit()
 
                 delete_user = "DELETE FROM `team_members` WHERE `user_id` = %s"
-                cursor.execute(delete_user, self.id)
+                cursor.execute(delete_user, self.db_id)
                 connection.commit()
         finally:
             connection.close()
@@ -182,9 +243,9 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "UPDATE `Пользователи` SET `Имя` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
+                # sql_request = "UPDATE `Пользователи` SET `Имя` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
                 sql_request = "UPDATE `users` SET `name` = %s WHERE `user_id` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, (self.name, self.id))
+                cursor.execute(sql_request, (self.name, self.db_id))
                 connection.commit()
         finally:
             connection.close()
@@ -193,41 +254,30 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "UPDATE `Пользователи` SET `User_name` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
+                # sql_request = "UPDATE `Пользователи` SET `User_name` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
                 sql_request = "UPDATE `users` SET `tg_username` = %s WHERE `user_id` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, (self.username, self.id))
+                cursor.execute(sql_request, (self.username, self.db_id))
                 connection.commit()
         finally:
             connection.close()
 
-    """def add_surname(self):
+    def add_group(self):
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "UPDATE `Пользователи` SET `Фамилия` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
-                sql_request = "UPDATE `Пользователи` SET `Фамилия` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, (self.surname, self.id))
+                sql_request = "UPDATE `users` SET `group_num` = %s WHERE `user_id` = %s"  # строка для SQL-запроса
+                cursor.execute(sql_request, (self.group, self.db_id))
                 connection.commit()
         finally:
-            connection.close()"""
-
-    """def add_group(self):
-        connection = connect_to_db()
-        try:
-            with connection.cursor() as cursor:
-                sql_request = "UPDATE `Пользователи` SET `Группа` = %s WHERE `Ид` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, (self.group, self.id))
-                connection.commit()
-        finally:
-            connection.close()"""
+            connection.close()
 
     def get_role_from_bd(self):
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "SELECT `Роль` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"  # строка для SQL-запроса
+                # sql_request = "SELECT `Роль` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `role` FROM `team_members` WHERE `user_id` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, self.id)
+                cursor.execute(sql_request, self.db_id)
                 role = cursor.fetchone()
                 connection.commit()
                 return role['role']
@@ -235,12 +285,13 @@ class User:
             connection.close()
 
     def get_teamname_from_bd(self):
+        self.set_db_id()
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "SELECT `Команда` FROM `Пользователи` WHERE `Ид` = %s"  # строка для SQL-запроса
-                sql_request = "SELECT `team_name` FROM `teams` WHERE `team_id` IN (SELECT `team_id` FROM `team_members` WHERE `user_id` = %s)"  # строка для SQL-запроса
-                cursor.execute(sql_request, self.id)
+                # sql_request = "SELECT `Команда` FROM `Пользователи` WHERE `Ид` = %s"  # строка для SQL-запроса
+                sql_request = "SELECT `team_name` FROM `teams` WHERE `team_id` = (SELECT `team_id` FROM `team_members` WHERE `user_id` = %s)"  # строка для SQL-запроса
+                cursor.execute(sql_request, self.db_id)
                 tmnm = cursor.fetchone()
                 connection.commit()
                 return tmnm['team_name']
@@ -249,13 +300,14 @@ class User:
 
     # Метод для проверки наличия пользователя в команде
     def is_in_team(self):
+        self.set_db_id()
         connection = connect_to_db()
         check = ''
         try:
             with connection.cursor() as cursor:
-                #user_from_db = "SELECT `Команда` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"
+                # user_from_db = "SELECT `Команда` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"
                 user_from_db = "SELECT `team_id` FROM `team_members` WHERE `user_id` = %s"
-                cursor.execute(user_from_db, self.id)
+                cursor.execute(user_from_db, self.db_id)
                 check = cursor.fetchall()
                 connection.commit()
         finally:
@@ -269,9 +321,9 @@ class User:
         check = ''
         try:
             with connection.cursor() as cursor:
-                #user_from_db = "SELECT `Название` FROM `Команды` WHERE `Ид` = %s"
+                # user_from_db = "SELECT `Название` FROM `Команды` WHERE `Ид` = %s"
                 user_from_db = "SELECT `team_name` FROM `teams` WHERE `admin_user_id` = %s"
-                cursor.execute(user_from_db, self.id)
+                cursor.execute(user_from_db, self.db_id)
                 check = cursor.fetchall()
                 connection.commit()
         finally:
@@ -284,7 +336,6 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-                #sql_request = "SELECT `Команда` FROM `Коды` WHERE `Код` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `team_id` FROM `team_members` WHERE `invite_code` = %s"  # строка для SQL-запроса
                 cursor.execute(sql_request, code)
                 result = cursor.fetchall()
@@ -299,7 +350,7 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-                #sql_request = "SELECT `Команда` FROM `Коды` WHERE `Код` = %s"  # строка для SQL-запроса
+                # sql_request = "SELECT `Команда` FROM `Коды` WHERE `Код` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `team_name` FROM `teams` WHERE `team_id` IN (SELECT `team_id` FROM `team_members` WHERE `invite_code` = %s)"  # строка для SQL-запроса
                 cursor.execute(sql_request, code)
                 result = cursor.fetchall()
@@ -309,12 +360,11 @@ class User:
         finally:
             connection.close()
 
-
     def get_role_using_code(self, code):
         connection = connect_to_db()
         try:
             with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-                #sql_request = "SELECT `Роль` FROM `Коды` WHERE `Код` = %s"  # строка для SQL-запроса
+                # sql_request = "SELECT `Роль` FROM `Коды` WHERE `Код` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `role` FROM `team_members` WHERE `invite_code` = %s"  # строка для SQL-запроса
                 cursor.execute(sql_request, code)
                 result = cursor.fetchall()
@@ -328,9 +378,9 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "UPDATE `Пользователи` SET `Ид` = %s WHERE `Команда` = %s AND `Роль` = %s"  # строка для SQL-запроса
-                sql_request = "UPDATE `users` SET `user_id` = %s WHERE `user_id` IN (SELECT `user_id` FROM `team_members` WHERE `team_id` IN (SELECT `team_id` FROM `teams` WHERE `team_name` = %s) AND `role` = %s)"  # строка для SQL-запроса
-                cursor.execute(sql_request, (self.id, self.teamname, self.role))
+                # sql_request = "UPDATE `Пользователи` SET `Ид` = %s WHERE `Команда` = %s AND `Роль` = %s"  # строка для SQL-запроса
+                sql_request = "UPDATE `users` SET `tg_id` = %s WHERE `user_id` IN (SELECT `user_id` FROM `team_members` WHERE `team_id` IN (SELECT `team_id` FROM `teams` WHERE `team_name` = %s) AND `role` = %s)"  # строка для SQL-запроса
+                cursor.execute(sql_request, (self.tg_id, self.teamname, self.role))
                 connection.commit()
         finally:
             connection.close()
@@ -339,15 +389,14 @@ class User:
         connection = connect_to_db()
         try:
             with connection.cursor() as cursor:
-                #sql_request = "SELECT `Имя` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"  # строка для SQL-запроса
+                # sql_request = "SELECT `Имя` FROM `Пользователи` WHERE `Ид` = %s or `User_name` = %s"  # строка для SQL-запроса
                 sql_request = "SELECT `name` FROM `users` WHERE `user_id` = %s"  # строка для SQL-запроса
-                cursor.execute(sql_request, self.id)
+                cursor.execute(sql_request, self.db_id)
                 role = cursor.fetchone()
                 connection.commit()
                 return role['name']
         finally:
             connection.close()
-
 
     # def add_report(self):
     #     connection = connect_to_db()
