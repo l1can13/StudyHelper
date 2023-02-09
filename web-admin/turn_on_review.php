dd168d77be00e799f9ec67e0d646be80821dca02
l1can13 <a.a.soroka05@gmail.com>
2023-02-09 19:58:36 +0400

    php fixes

--- web-admin/turn_on_review.php
+++ web-admin/turn_on_review.php
@@ -1,7 +1,7 @@
 <?php header('Access-Control-Allow-Origin: *');
 
 $connection = ssh2_connect('studhelper.online', 22);
-ssh2_auth_password($connection, 'root', '9hfeygRJw~-$');
+ssh2_auth_password($connection, 'root', '4E%$@7-#?$|@');
 
 ssh2_exec($connection, 'sudo systemctl stop tgbot');
 ssh2_exec($connection, 'sudo systemctl start tgbot_review');
